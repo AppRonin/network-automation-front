a2d48a3024732a6551a8ca430d90822e87a8f4f1
Victor <victorfariasbezerra12@gmail.com>
2025-12-14 16:27:45 -0300

    Small Fix

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -38,81 +38,86 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        width: 400,
-        height: 320,
-        padding: EdgeInsets.symmetric(horizontal: 24),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.grey.shade300),
-          borderRadius: BorderRadius.circular(12),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1), // Shadow color with opacity
-              offset: Offset(0, 4), // X, Y offset
-              blurRadius: 6, // Blur radius
-              spreadRadius: 0, // Spread radius
-            ),
-            BoxShadow(
-              color: Colors.black.withOpacity(0.06),
-              offset: Offset(0, 2),
-              blurRadius: 4,
-              spreadRadius: 0,
-            ),
-          ],
-        ),
-        child: Column(
-          children: [
-            SizedBox(height: 24),
-            Text(
-              "Login",
-              style: GoogleFonts.inter(
-                fontSize: 24,
-                fontWeight: FontWeight.w700,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 64),
+        child: Container(
+          width: 400,
+          height: 320,
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.grey.shade300),
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(
+                  0.1,
+                ), // Shadow color with opacity
+                offset: Offset(0, 4), // X, Y offset
+                blurRadius: 6, // Blur radius
+                spreadRadius: 0, // Spread radius
               ),
-            ),
-            SizedBox(height: 24),
-            CustomInput(
-              label: "Usuario",
-              placeholder: "Digite seu usuário",
-              isPassword: false,
-              controller: userController,
-            ),
-            SizedBox(height: 16),
-            CustomInput(
-              label: "Password",
-              placeholder: "*********",
-              isPassword: true,
-              controller: passwordController,
-            ),
-            SizedBox(height: 24),
+              BoxShadow(
+                color: Colors.black.withOpacity(0.06),
+                offset: Offset(0, 2),
+                blurRadius: 4,
+                spreadRadius: 0,
+              ),
+            ],
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 24),
+              Text(
+                "Login",
+                style: GoogleFonts.inter(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(height: 24),
+              CustomInput(
+                label: "Usuario",
+                placeholder: "Digite seu usuário",
+                isPassword: false,
+                controller: userController,
+              ),
+              SizedBox(height: 16),
+              CustomInput(
+                label: "Password",
+                placeholder: "*********",
+                isPassword: true,
+                controller: passwordController,
+              ),
+              SizedBox(height: 24),
 
-            // Login Button
-            MouseRegion(
-              cursor: SystemMouseCursors.click,
-              child: GestureDetector(
-                onTap: loading ? null : handleLogin,
-                child: Container(
-                  width: double.infinity,
-                  height: 38,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Entrar",
-                      style: GoogleFonts.inter(
-                        fontSize: 14,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
+              // Login Button
+              MouseRegion(
+                cursor: SystemMouseCursors.click,
+                child: GestureDetector(
+                  onTap: loading ? null : handleLogin,
+                  child: Container(
+                    width: double.infinity,
+                    height: 38,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Entrar",
+                        style: GoogleFonts.inter(
+                          fontSize: 14,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
