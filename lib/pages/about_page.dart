28b918d21ad92ac4e3030dcf18111989c4f1eb6a
Victor <victorfariasbezerra12@gmail.com>
2025-12-14 16:24:33 -0300

    AboutPage

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,10 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:network_automation/widgets/bullet.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Sobre"));
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 64),
+        child: SizedBox(
+          width: 600,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Sobre',
+                style: GoogleFonts.inter(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                "Esta aplicação foi criada para facilitar o trabalho do time de suporte de rede.",
+                style: GoogleFonts.inter(fontSize: 14),
+              ),
+              SizedBox(height: 16),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey.shade300),
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      offset: Offset(0, 4),
+                      blurRadius: 6,
+                      spreadRadius: 0,
+                    ),
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.06),
+                      offset: Offset(0, 2),
+                      blurRadius: 4,
+                      spreadRadius: 0,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "O que ela faz?",
+                      style: GoogleFonts.inter(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 24),
+                    Bullet(
+                      text: "Automatiza atividades repetitivas do suporte.",
+                    ),
+                    Bullet(text: "Facilita diagnósticos e conferencias."),
+                    Bullet(text: "Padroniza processos para evitar erros."),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey.shade300),
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      offset: Offset(0, 4),
+                      blurRadius: 6,
+                      spreadRadius: 0,
+                    ),
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.06),
+                      offset: Offset(0, 2),
+                      blurRadius: 4,
+                      spreadRadius: 0,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Principais benefícios",
+                      style: GoogleFonts.inter(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 24),
+                    Bullet(text: "Mais rapidez."),
+                    Bullet(text: "Menos erros."),
+                    Bullet(text: "Maior produtividade."),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                "@ 2025 • AppRonin",
+                style: GoogleFonts.inter(fontSize: 14, color: Colors.grey),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/bullet.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Bullet extends StatelessWidget {
+  final String text;
+
+  const Bullet({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Text('• ', style: TextStyle(fontSize: 24)),
+        Text(text, style: GoogleFonts.inter(fontSize: 14)),
+      ],
+    );
+  }
+}
